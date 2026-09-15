cba5551ddc810022837bee010ae79660f69b8b8f
masago1 <costindavid719@gmail.com>
2026-09-15 22:17:41 +0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Găsește chiria potrivită pentru facultatea ta 🏠</h1>
+
+      <p>
+        Apartamente și camere aproape de cele mai mari centre universitare
+        din România.
+      </p>
+
+      <button>Caută o chirie</button>
+    </main>
+  );
+}
