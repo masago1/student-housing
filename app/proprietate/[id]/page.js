5f63a674f9dba3f758de35ad182c0749c689bdd0
masago1 <costindavid719@gmail.com>
2026-09-17 18:09:17 +0300

    Update page.js

--- app/proprietate/[id]/page.js
+++ app/proprietate/[id]/page.js
@@ -44,7 +44,7 @@ export default async function PropertyPage({ params }) {
                             letterSpacing: "-1px",
                         }}
                     >
-                        StudentHousing
+                        shaus
                     </a>
                 </header>
 
