ca4dce76f4b1be1444517221c8a4a4ae3e832716
masago1 <costindavid719@gmail.com>
2026-09-17 19:33:14 +0300

    Update ApproximateLocationMap.js

--- app/components/ApproximateLocationMap.js
+++ app/components/ApproximateLocationMap.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef } from "react";
 import mapboxgl from "mapbox-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
+
 
 const MAPBOX_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
 
